19e0f2bc58af74d019063fe100a4f8f9db756381
SudhakarDatt <sudhakar.datt@gmail.com>
2026-05-10 09:44:53 +0530

    Add/update WeatherAPI/Security/IEncryptionService.cs

---
+++ WeatherAPI/Security/IEncryptionService.cs
@@ -0,0 +1,9 @@
+namespace WeatherAPI.Security;
+
+public interface IEncryptionService
+{
+    string Encrypt(string plainText);
+    string Decrypt(string cipherText);
+    byte[] EncryptBytes(byte[] plainBytes);
+    byte[] DecryptBytes(byte[] cipherBytes);
+}
