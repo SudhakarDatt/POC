8ab209a8f9152da80cbaf9999b7696e5fe85d830
SudhakarDatt <sudhakar.datt@gmail.com>
2026-05-10 09:45:51 +0530

    Add/update WeatherAPI/Models/WeatherResponse.cs

---
+++ WeatherAPI/Models/WeatherResponse.cs
@@ -0,0 +1,15 @@
+namespace WeatherAPI.Models;
+
+public class WeatherResponse
+{
+    public int Id { get; set; }
+    public string City { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public double Temperature { get; set; }
+    public int Humidity { get; set; }
+    public double WindSpeed { get; set; }
+    public string Condition { get; set; } = string.Empty;
+    public double Pressure { get; set; }
+    public double Visibility { get; set; }
+    public DateTime Timestamp { get; set; }
+}
