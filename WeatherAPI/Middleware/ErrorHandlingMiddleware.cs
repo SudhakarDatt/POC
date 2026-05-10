243719ed9b42d35cc4b4a4018ad5280eddfcd714
SudhakarDatt <sudhakar.datt@gmail.com>
2026-05-10 09:45:58 +0530

    Add/update WeatherAPI/Middleware/ErrorHandlingMiddleware.cs

---
+++ WeatherAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using System.Text.Json;
+
+namespace WeatherAPI.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        await context.Response.WriteAsync(JsonSerializer.Serialize(new { error = exception.Message }));
+    }
+}
